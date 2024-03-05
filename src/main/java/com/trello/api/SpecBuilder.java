8a5b8b9f327d4d59d688ef4d3d196abd74758966
My Nguyen - 48294 <my.tra.nguyen@logigear.com>
2024-03-05 13:46:03 +0700

    Try customizing reports

--- src/main/java/com/trello/api/SpecBuilder.java
+++ src/main/java/com/trello/api/SpecBuilder.java
@@ -1,11 +1,17 @@
 package com.trello.api;
 
+import com.trello.utils.RestAssuredRequestFilter;
 import com.trello.utils.config.PropertiesConfigLoader;
 import com.trello.utils.config.TrelloApiRoute;
 import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.config.LogConfig;
+import io.restassured.config.RestAssuredConfig;
 import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
@@ -13,19 +19,23 @@ import io.restassured.specification.ResponseSpecification;
 public class SpecBuilder {
 
     public static RequestSpecification getRequestSpec(){
+//        RestAssured.config = RestAssured.config()
+//                .logConfig(LogConfig.logConfig()
+//                        .enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)
+//                        .enablePrettyPrinting(true));
+//        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+
         return new RequestSpecBuilder().
                 setBaseUri(TrelloApiRoute.trelloApiPath().getBaseUrl()).
                 setBasePath(TrelloApiRoute.trelloApiPath().getBasePath()).
                 addQueryParam("token", PropertiesConfigLoader.getInstance().getProperty("app_token"))
                 .addQueryParam("key", PropertiesConfigLoader.getInstance().getProperty("app_key"))
-                .setContentType(ContentType.JSON)
-                .log(LogDetail.ALL).
-                build().filter(new AllureRestAssured());
+//                .setContentType(ContentType.JSON).build().filter(new AllureRestAssured());
+                .setContentType(ContentType.JSON).build();
     }
 
     public static ResponseSpecification getResponseSpec(){
         return new ResponseSpecBuilder().
-                log(LogDetail.ALL).
                 build();
     }
 }
--- src/test/java/com/trello/base/Listeners.java
+++ src/test/java/com/trello/base/Listeners.java
@@ -1,8 +1,55 @@
 package com.trello.base;
 
+import com.trello.utils.RestAssuredRequestFilter;
+import io.qameta.allure.Attachment;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestResult;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class Listeners implements ITestListener  {
+    private ByteArrayOutputStream request = new ByteArrayOutputStream();
 
+    private ByteArrayOutputStream response = new ByteArrayOutputStream();
+
+    private PrintStream requestVar = new PrintStream(request, true);
+    private PrintStream responseVar = new PrintStream(response, true);
+
+    public void onStart(final ITestContext iTestContext) {
+        RestAssured.filters(new ResponseLoggingFilter(LogDetail.ALL, responseVar),
+                new RequestLoggingFilter(LogDetail.ALL, requestVar));
+    }
+
+    public void onTestSuccess(final ITestResult iTestResult) {
+        logRequest(request);
+        logResponse(response);
+    }
+
+
+    public void onTestFailure(final ITestResult iTestResult) {
+        logRequest(request);
+        logResponse(response);
+    }
+
+    @Attachment(value = "request")
+    private byte[] logRequest(final ByteArrayOutputStream stream) {
+        return attach(stream);
+    }
+
+    @Attachment(value = "response")
+    private byte[] logResponse(final ByteArrayOutputStream stream) {
+        return attach(stream);
+    }
+
+    private byte[] attach(final ByteArrayOutputStream log) {
+        final byte[] array = log.toByteArray();
+        log.reset();
+        return array;
+    }
 }
 
