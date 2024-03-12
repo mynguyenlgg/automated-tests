6f650abe9b7644d0e152d1a33c069e165f67a183
mynguyenlgg <my.tra.nguyen@agest.vn>
2024-03-12 13:52:58 +0700

    Update params constructor

--- src/main/java/com/client/services/ParamsBuilder.java
+++ src/main/java/com/client/services/ParamsBuilder.java
@@ -10,23 +10,26 @@ import java.util.Map;
 @Setter
 @Getter
 @Builder
-public class ParamsBuilder<String, V> {
-    private Map<String, V> pathParams;
-    private Map<String, V> queryParams;
+public class ParamsBuilder<String, T> {
+    private Map<String, T> pathParams;
+    private Map<String, T> queryParams;
 
-    public ParamsBuilder<String, V> pathParam(String key, V value) {
-        if (this.pathParams == null) {
-            this.pathParams = new HashMap<>();
+    public ParamsBuilder(Map<String, T> pathParams, Map<String, T> queryParams) {
+        this.pathParams = pathParams != null ? new HashMap<>(pathParams) : new HashMap<>();
+        this.queryParams = queryParams != null ? new HashMap<>(queryParams) : new HashMap<>();
+    }
+
+    public ParamsBuilder<String, T> pathParam(String key, T value) {
+        if (this.pathParams != null) {
+            this.pathParams.put(key, value);
         }
-        this.pathParams.put(key, value);
         return this;
     }
 
-    public ParamsBuilder<String, V> queryParam(String key, V value) {
-        if (this.queryParams == null) {
-            this.queryParams = new HashMap<>();
+    public ParamsBuilder<String, T> queryParam(String key, T value) {
+        if (this.queryParams != null) {
+            this.queryParams.put(key, value);
         }
-        this.queryParams.put(key, value);
         return this;
     }
 }
