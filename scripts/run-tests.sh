429b5c4cabb36fbb3b2a59958a59ca36d58a9163
My Nguyen - 48294 <my.tra.nguyen@logigear.com>
2024-03-26 10:05:46 +0700

    Update code

---
+++ scripts/run-tests.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ta execute -ls "trialkey:OSEB0S-VRN06-GVDA0-CEHUX-H1NO9" -rep "SampleRepository" -prj "Scrum Board" -u "administrator" -p "04848504C58" -t "/Functional Tests/Login validation" -rs "192.168.191.215:53400" -udf "build number=" -r "Login validation" -html "/home/truong/TestArchitect/results" -subfld "1" -subhtml "0" -tscript "{INSTALL_DIR}/binclient/taplayback.sh" -tpath "{INSTALL_DIR}/binclient/taplayback.sh"
